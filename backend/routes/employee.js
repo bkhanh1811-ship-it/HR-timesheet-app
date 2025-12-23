51787d6901a816f29ff666ff496dba00b26223c3
bkhanh1811-ship-it <bkhanh1811@gmail.com>
2025-12-23 15:48:23 +0700

    Replace Employee routes with Attendance routes

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const EmployeeController = require("../controllers/employee");
+const AttendanceController = require("../controllers/attendanceController");
 
-router.get("/", EmployeeController.getEmployees); // This is the route that the frontend will call to get all employees
-router.get("/:id", EmployeeController.getEmployeeById); // This is the route that the frontend will call to get an employee by id
-router.post("/", EmployeeController.addEmployee); // This is the route that the frontend will call to add an employee
+router.post("/", AttendanceController.upsertAttendance);
+router.get("/", AttendanceController.getAttendanceByMonth);
 
 module.exports = router;
